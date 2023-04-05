fa470c9392e2189dcee64782fed287999553ccec
muhfarrasm <muhammadfarrasm@gmail.com>
2023-04-05 15:25:59 +0700

    int main for 1

--- Structure3/Structure3.cpp
+++ Structure3/Structure3.cpp
@@ -16,5 +16,31 @@ struct Mahasiswa
     int umur;
 };
 
+int main()
+{
+    Mahasiswa mhs[5];
 
+
+    for (int i = 0; i < 5; i++) {
+
+
+        cout << "Masukkan NIM = ";
+        cin.getline(mhs[i].NIM, 12);
+        cout << "Masukkan Nama = ";
+        cin.getline(mhs[i].Nama, 20);
+        cout << "Alamat : ";
+        cout << "\nMasukan Desa = ";
+        cin.getline(mhs[i].Alamat.Desa, 20);
+        cout << "\tMasukkan Kecamata = ";
+        cin.getline(mhs[i].Alamat.Kecamatan, 20);
+        cout << "\tMasukkan Kota = ";
+        cin.getline(mhs[i].Alamat.Kota, 20);
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+    }
+
+
+   
+
+}
 
