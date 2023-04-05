af4d10a9688f8a2c6d53c8af32787e44be99309f
muhfarrasm <muhammadfarrasm@gmail.com>
2023-04-05 15:02:06 +0700

    int main

--- Structure2/Structure2.cpp
+++ Structure2/Structure2.cpp
@@ -17,4 +17,33 @@ struct Mahasiswa
 };
 
 
+int main()
+{
+    Mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.NIM;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.Nama;
+    cout << "Alamat : ";
+    cout << "\nMasukan Desa = ";
+    cin >> mhs.Alamat.Desa;
+    cout << "\nMasukkan Kecamata = ";
+    cin >> mhs.Alamat.Kecamatan;
+    cout << "\nMasukkan Kota = ";
+    cin >> mhs.Alamat.Kota;
+    cout << "Masukkan umur = ";
+    cin >> mhs.umur;
+
+    cout << "\nNIM = " << mhs.NIM;
+    cout << "\nNama = " << mhs.Nama;
+    cout << "\nAlamat Desa = " << mhs.Alamat.Desa;
+    cout << "\nAlamat Kecamatan = " << mhs.Alamat.Kecamatan;
+    cout << "\nAlamat Kota = " << mhs.Alamat.Kota;
+
+    cout << "\nUmur = " << mhs.umur;
+
+}
+
+
 
